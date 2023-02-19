93c694606ab727329b13665674f26bb9f92ca496
Elias Eshetu Mammo <101708744+Blacklio@users.noreply.github.com>
2023-02-19 15:04:49 +0000

    Update review.py
    review

--- models/review.py
+++ models/review.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python3
-"""This module creates a Review class"""
+##!/usr/bin/python3
+"""Module base_model
+
+This Module contains a definition for Amenity Class
+"""
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
+    """A class that represents a review
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
 
     place_id = ""
     user_id = ""
